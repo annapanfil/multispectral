9b464c03855ed681e36dbbecbcfd5d672eb494ea
Anna Panfil <134panka@gmail.com>
2025-03-14 09:10:32 +0100

    fix(create_dataset): run with RGB

--- src/run_create_dataset.py
+++ src/run_create_dataset.py
@@ -8,7 +8,9 @@ import mlflow
 import pandas as pd
 
 def run_job(job):
-    print(" ".join(job))
+    if None not in job:
+        print(" ".join(job))
+    else: print(job)
     subprocess.run(job, check=True)
 
 if __name__ == "__main__":
@@ -18,34 +20,47 @@ if __name__ == "__main__":
     df = mlflow.search_runs(experiment_ids=[experiment_id])
 
     # GET FORMULAS
-    field_names = ["tags.mlflow.runName", "params.dataset", "params.final_formula"]
+    field_names = ["tags.mlflow.runName", "params.dataset", "params.final_formula", "tags.discard"]
     selected_columns = df[field_names]
 
-    baseline_rows = pd.DataFrame([["rndwi", None, "N # G"],
-                    ["meanre", None, "(4#1) + (4#0)"],
-                    ["RGB", None, None]], columns=field_names)
+    baseline_rows = pd.DataFrame([["rndwi", None, "N # G", "false"],
+                    ["meanre", None, "(4#1) + (4#0)", "false"],
+                    ["RGB", None, None, "false"]], columns=field_names)
 
     selected_columns = pd.concat([selected_columns, baseline_rows], ignore_index=True)
 
+    pd.set_option('display.max_rows', None)  # Show all rows
     print(selected_columns)
+
+    print(selected_columns[selected_columns["tags.discard"] != "true"])
 
     # CREATE BASE DATASETS
     for _, row in  selected_columns.iterrows():
-        name, ds, formula = row
+        name, ds, formula, discard = row
+        if discard == "true":
+            print(name, "is discarded")
+            continue
+
+        if name == "RGB":
+            run_job(["python3", "-m", "detection.create_RGB_dataset", "-n", "RGB_whole_random"])
+            run_job(["python3", "-m", "detection.create_RGB_dataset", "-e", "mandrac", "-n", "RGB_pool_random"])
+            continue
+
         if formula is None:
             print(name, "is not finished")
             continue
 
         # train and test on whole
         run_job(["python3", "-m", "detection.create_dataset", 
-            "-n", f"{ds}{'-' if ds else ''}{name.replace(' ', '-')}_whole_random", 
+            "-n", f"{ds if ds else ''}{'-' if ds else ''}{name.replace(' ', '-')}_whole_random", 
             "-f", formula])
         
         # train on whole, test on pool
         run_job(["python3", "-m", "detection.create_dataset", 
             "-e", "mandrac",
-            "-n", f"{ds}{'-' if ds else ''}{name.replace(' ', '-')}_whole_random", 
+            "-n", f"{ds if ds else ''}{'-' if ds else ''}{name.replace(' ', '-')}_pool_random", 
             "-f", formula])
+
 
     # MERGE INDEX AND RGB DATASETS
     # train on whole idx + RGB, test on whole
