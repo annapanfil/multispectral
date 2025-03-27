ca964f9bad545f12c6f68c355e625abc3212ebda
Anna Panfil <134panka@gmail.com>
2025-03-27 09:08:48 +0100

    feat: add inside gt boxes for one image

--- src/detection/metric.py
+++ src/detection/metric.py
@@ -10,10 +10,61 @@ import matplotlib.pyplot as plt
 from detection.yolo import read_ground_truth
 
 ds_path = "/home/anna/Datasets/created/pool-form1_pool-3-channels_random/"
-yaml =  f"{ds_path}/pool-form1_pool-3-channels_random.yaml" # "detection/data.yaml"
-split = "val" #"example"
+FULL_DS = False
+
+if FULL_DS:
+    #test whole val ds
+    yaml = f"{ds_path}/pool-form1_pool-3-channels_random.yaml"
+    split = "val"
+else: 
+    yaml =  "detection/data.yaml"
+    split = "example"
+
+global_matched = [] # for showing them in the final image
+
+def is_inside(bb, bbs, idx_filter=None):
+    """
+    Check if bounding box is inside any of the given bounding boxes.
+
+    Args:
+        bb (int): Bounding box index.
+        bbs (torch.Tensor): Tensor of shape (N, 4) representing bounding boxes where each bounding box is of the
+            format: (x1, y1, x2, y2).
+        idx_filter (torch.Tensor): Tensor of shape (M,) representing indices of bounding boxes to consider.
+
+    Returns:
+        int: index of the outside bounding box, None if not found.
+    """
+    for i, bbox in enumerate(bbs):
+        if i in idx_filter:
+            if bbox[0] <= bb[0] and bbox[1] <= bb[1] and bbox[2] >= bb[2] and bbox[3] >= bb[3]:
+                return i
+    return None
+        
 
 class MyValidator(DetectionValidator):
+    def __init__(self, dataloader=None, save_dir=None, pbar=None, args=None, _callbacks={}):
+        """Initialize detection model with necessary variables and settings.""" 
+        
+        _callbacks["on_val_batch_end"].append(self._add_new_rows)
+        super().__init__(dataloader, save_dir, pbar, args, _callbacks)
+
+        self.additional_pred_bbs = []
+        self.additional_confidences = []
+        self.additional_pred_cls = []
+        
+    def _add_new_rows(self, _):
+        print("\nAdding new rows\n")
+        if self.additional_pred_bbs != []:
+            self.additional_pred_bbs = torch.cat(self.additional_pred_bbs, dim=0)  # Stack along dim=0
+            self.stats["tp"][-1] = torch.cat((self.stats["tp"][-1], self.additional_pred_bbs), dim=0)
+            self.stats["conf"][-1] = torch.cat((self.stats["conf"][-1], torch.cat(self.additional_confidences, dim=0)), dim=0)
+            self.stats["pred_cls"][-1] = torch.cat((self.stats["pred_cls"][-1], torch.cat(self.additional_pred_cls, dim=0)), dim=0)
+
+            self.additional_pred_bbs = []
+            self.additional_confidences = []
+            self.additional_pred_cls = []
+
     def _process_batch(self, detections, gt_bboxes, gt_cls):
         """
         Return correct prediction matrix.
@@ -33,8 +84,29 @@ class MyValidator(DetectionValidator):
             intermediate representation used for evaluating predictions against ground truth.
         """
         iou = box_iou(gt_bboxes, detections[:, :4])
-        return self.match_predictions(detections[:, 5], gt_cls, iou)
+        correct, matches = self.match_predictions(detections[:, 5], gt_cls, iou)
 
+        detections_thr = detections[detections[:, 4] > 0.5]
+        matched_gt = matches[matches[:, 1] < detections_thr.shape[0]][:, 0]
+        not_matched_gt = np.setdiff1d(np.arange(gt_cls.shape[0]), matched_gt)        
+        
+        global global_matched
+        global_matched = matched_gt.tolist()
+
+        for bb in not_matched_gt:
+            outside_bb = is_inside(gt_bboxes[bb], gt_bboxes, matched_gt) 
+            if outside_bb:
+                # match the prediction from the outside_bb to the bb
+                prediction_idx = matches[matches[:, 0] == outside_bb, 1][0]
+                self.additional_pred_bbs.append(correct[prediction_idx].unsqueeze(0))
+                self.additional_confidences.append(detections[prediction_idx, 4].unsqueeze(0))
+                self.additional_pred_cls.append(detections[prediction_idx, 5].unsqueeze(0))
+            
+                global_matched.append(bb) 
+
+        return correct
+    
+    
     def match_predictions(self, pred_classes, true_classes, iou, use_scipy=False):
         """
         Matches predictions to ground truth objects (pred_classes, true_classes) using IoU.
@@ -64,38 +136,21 @@ class MyValidator(DetectionValidator):
                         # matches = matches[matches[:, 2].argsort()[::-1]]
                         matches = matches[np.unique(matches[:, 0], return_index=True)[1]]
                     correct[matches[:, 1].astype(int), i] = True
-        return torch.tensor(correct, dtype=torch.bool, device=pred_classes.device)
+                if i == 0:
+                    max_matches = matches
+        return torch.tensor(correct, dtype=torch.bool, device=pred_classes.device), max_matches
 
 # Load model
 model = YOLO("/home/anna/code/multispectral/src/detection/pool-form1_pool-3-channels_random_best.pt")
-
-# # show image
-# results = model.predict(source=f"{ds_path}images/{split}/", conf=0.3, save=False)
-# gt = read_ground_truth(results)
-
-# # Add annots to each image
-# for result, gt in zip(results, gt):
-#     result.names[0] = ""
-#     img = result.plot(conf=True, line_width=1, font_size=5)
-#     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-#     for box in gt:
-#         x1, y1, x2, y2 = box
-#         img = cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 1)
-
-# plt.imshow(img)
-# plt.show()
 
 # get metrics
 our_results = model.val(data=yaml, validator=MyValidator)
 ap50_our = our_results.results_dict["metrics/mAP50(B)"]
 ap5095_our = our_results.results_dict['metrics/mAP50-95(B)']
 
-
 results = model.val(data=yaml, validator=DetectionValidator)
 ap50_yolo = results.results_dict["metrics/mAP50(B)"]
 ap5095_yolo = results.results_dict['metrics/mAP50-95(B)']
-
 
 plt.plot(np.linspace(50, 95, len(results.box.all_ap[0])), results.box.all_ap[0], color="red", 
          label=f"original (AP50={ap50_yolo:.3f}, AP50-95={ap5095_yolo:.3f})")
@@ -112,5 +167,24 @@ print( "---------------------------")
 print(f"Original | {ap50_yolo:.4f} | {ap5095_yolo:.4f}")
 print(f"Improved | {ap50_our:.4f} | {ap5095_our:.4f}")
 
+if not FULL_DS:
+    # show image
+    results = model.predict(source=f"{ds_path}images/{split}/", conf=0.3, save=False)
+    gt = read_ground_truth(results)
 
+    # Add annots to each image
+    for result, gt in zip(results, gt):
+        result.names[0] = ""
+        img = result.plot(conf=True, line_width=1, font_size=5)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
+        for box in gt:
+            x1, y1, x2, y2 = box
+            img = cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 1)
+        
+        for bb in global_matched:
+            x1, y1, x2, y2 = gt[bb]
+            img = cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 1)
+
+    plt.imshow(img)
+    plt.show()
